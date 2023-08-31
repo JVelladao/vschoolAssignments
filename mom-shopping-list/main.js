1b15127b91d50e8b0ed00a3f978a89a4c19aaee8
Joe Velladao <jvelladao@gmail.com>
2023-08-31 15:54:23 -0700

    mom's shopping list just need to add the button event listener

--- mom-shopping-list/main.js
+++ mom-shopping-list/main.js
@@ -2,6 +2,10 @@ const form = document.mom
 
 //submit event-by default-refreshes your page.
 
+const li3 =document.createElement('button');
+li3.textContent="X";
+
+
 form.addEventListener("submit",function(event){
    
     event.preventDefault();     //this is a method, thus you need ().Note:this will be the first command in any submit/form function.
@@ -9,40 +13,18 @@ form.addEventListener("submit",function(event){
     const groceryInput = form.grocery.value;  //saves the value inputed into the tag   progression: form(document.mom) to input (tag) to value(actual stuff typed in)
     form.grocery.value = ""; //clears out the form for the user after they click submit. Note we can do this on this line becuase we captured the users input in line 9.
     
-    
-    //const button =document.createElement('button');
-    //button.textContent= 'X';
-    //document.getElementById("li1").append(button);
-    
-    
-   // var xray= "X"
-
-    //const button =document.createElement('button');
-    //button.textContent = xray;
-    //document.getElementById("xnow").append(button);
-
-
-    const li1 =document.createElement('button');
-    li1.textContent= "x"; 
 
     const li2 =document.createElement('li');
-    li2.textContent= groceryInput;
-    document.getElementById("list").append(li2);
+    li2.textContent= "  " + groceryInput;
+   document.getElementById("list").append(li2);
 
+   const button =document.createElement('button');
+   button.textContent="X";
+   li2.prepend(button);
 
-    document.getElementById("list").append(li1);
-
-   
-
+ 
     
-
-   
-
   
-   
-    //setAttribute
-
-
 })
 
 //here is the issue as clearly defined as I can explain it. I figured out how to produce an X button, the desired gorcery content
