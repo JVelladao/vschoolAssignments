4139b8aa22a8a94c20dfe2ebbe5f2de59a041f71
Joe Velladao <jvelladao@gmail.com>
2023-09-06 12:41:19 -0700

    mom done

--- mom-shopping-list/main.js
+++ mom-shopping-list/main.js
@@ -8,24 +8,88 @@ li3.textContent="X";
 
 form.addEventListener("submit",function(event){
    
-    event.preventDefault();     //this is a method, thus you need ().Note:this will be the first command in any submit/form function.
-    //alert("It's working");      //to test the submit event...
-    const groceryInput = form.grocery.value;  //saves the value inputed into the tag   progression: form(document.mom) to input (tag) to value(actual stuff typed in)
-    form.grocery.value = ""; //clears out the form for the user after they click submit. Note we can do this on this line becuase we captured the users input in line 9.
+    event.preventDefault();     
+    const groceryInput = form.grocery.value;  
+    form.grocery.value = "";  
     
 
-    const li2 =document.createElement('li');
+    const li2 = document.createElement('li');
+
+    let listElement = document.getElementById('list')
+    console.log('list children', listElement.children)
+
     li2.textContent= "  " + groceryInput;
    document.getElementById("list").append(li2);
+    // Add classname
+
+
 
    const button =document.createElement('button');
    button.textContent="X";
    li2.prepend(button);
 
- 
+
+   list.addEventListener("click", function(event){
+     this.removeChild(li2);
+   });
+
+
+
+
+
+   
+   });
+
+/*
+let todoArr = []
+
+submit button 
+- add the todo to array
+- display an array using the list items in todoArr
+
+delete todo button
+- delete the todo
+- display the updated array
+
+display function
+- clears old todos
+- loops the todoArra
+   - todoArr for loop that creates elements for each item in todoArr.length
+
+
+A function that creates elements
+- create a list element for the item that's sent
+- 
+*/
+
+   
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
   
-})
+
+
+
 
 //here is the issue as clearly defined as I can explain it. I figured out how to produce an X button, the desired gorcery content
 // and a bullet point. Now all I need to do is align it all properly and fix an event listener to the button. Onwards and upwards.
